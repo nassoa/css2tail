6d04d6bde12711227b4322f9c4ababa62d535614
Nasoavina <nasoavina.m@gmail.com>
2025-04-29 19:48:52 +0200

    version reset v-03

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-// Interface pour les props du ThemeProvider
-interface ThemeProviderProps {
-  children: React.ReactNode;
-  [key: string]: any; // Pour permettre d'autres props
-}
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
